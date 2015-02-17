680fbacd7efe28723682beb0f6006af66a0eeb88
dream <dxs9642@gmail.com>
2015-02-17 21:45:41 +0800

    tired..........

--- app/src/main/java/com/dream/net/mobilesafe/MainActivity.java
+++ app/src/main/java/com/dream/net/mobilesafe/MainActivity.java
@@ -1,7 +1,10 @@
 package com.dream.net.mobilesafe;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.GridView;
 
 import com.dream.net.adapter.HomeGridViewAdapter;
@@ -22,6 +25,18 @@ public class MainActivity extends Activity{
         setContentView(R.layout.activity_main);
         gv_home_item = (GridView)findViewById(R.id.gv_home_item);
         gv_home_item.setAdapter(new HomeGridViewAdapter(this));
+        gv_home_item.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position){
+                    case 8:
+                        Intent intent = new Intent(MainActivity.this,SettingActivity.class);
+                        startActivity(intent);
+                        break;
+                }
 
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/dream/net/mobilesafe/SettingActivity.java
@@ -0,0 +1,17 @@
+package com.dream.net.mobilesafe;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by sunlongxiao on 2/17/15.
+ */
+public class SettingActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+
+    }
+}
