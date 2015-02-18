84828ae3c29bba919323a7adee309a9e8dc36ad7
dream <dxs9642@gmail.com>
2015-02-18 15:28:04 +0800

    the last commit before this year

--- app/src/main/java/com/dream/net/mobilesafe/MainActivity.java
+++ app/src/main/java/com/dream/net/mobilesafe/MainActivity.java
@@ -1,8 +1,11 @@
 package com.dream.net.mobilesafe;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
@@ -14,6 +17,7 @@ import com.dream.net.adapter.HomeGridViewAdapter;
  {33eecb5f-1a64-4b9f-a852-a4ef7327e0f5} */
 public class MainActivity extends Activity{
     private GridView gv_home_item;
+    private SharedPreferences sp;
 
 
 
@@ -23,12 +27,19 @@ public class MainActivity extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sp = getSharedPreferences("config",MODE_PRIVATE);
+
         gv_home_item = (GridView)findViewById(R.id.gv_home_item);
         gv_home_item.setAdapter(new HomeGridViewAdapter(this));
         gv_home_item.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 switch (position){
+                    case 0:
+                        showPhoneProtectDialog();
+                        break;
+
                     case 8:
                         Intent intent = new Intent(MainActivity.this,SettingActivity.class);
                         startActivity(intent);
@@ -39,4 +50,29 @@ public class MainActivity extends Activity{
             }
         });
     }
+
+
+    public void showPhoneProtectDialog(){
+
+        String hasSetted = sp.getString("PhoneProtectPassword","");
+        if (TextUtils.isEmpty(hasSetted)){
+            showCreatePwdDialog();
+        }else{
+            showInputPwdDialog();
+        }
+
+    }
+
+    public void showCreatePwdDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        View view = View.inflate(MainActivity.this, R.layout.linear_dialog_setup_pwd, null);
+        builder.setView(view);
+        builder.show();
+    }
+
+
+    public void showInputPwdDialog(){
+
+    }
+
 }
